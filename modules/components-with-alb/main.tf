eb3dc17410cc738eb2280cd7b794ed36f2e44514
abhishekdontukurti <dabhishek175@gmail.com>
2026-05-26 18:42:07 -0400

    Terraform-lbs- v7

--- modules/components-with-alb/main.tf
+++ modules/components-with-alb/main.tf
@@ -96,6 +96,10 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
   health_check {
     path  = "/health"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    interval = 5
+    timeout = 2
   }
 }
 resource "aws_lb" "main" {
